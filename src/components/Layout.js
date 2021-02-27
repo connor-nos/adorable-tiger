876a01b2f97b5f36c9de630c8fa75cdefcb970d2
Stackbit Code Editor <support@stackbit.com>
2021-02-27 15:52:03 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,7 +42,7 @@ export default class Body extends React.Component {
                     )}
                     <body className={'palette-' + _.get(this.props, 'pageContext.site.siteMetadata.color_scheme', null) + ' accent-' + _.get(this.props, 'pageContext.site.siteMetadata.accent_color', null)} />
                 </Helmet>
-                <img src="../../../static/images/thank-you.jpg">
+                <img src="../../../static/images/thank-you.jpg"></img>
                 <Header {...this.props} />
                 <div id="page" className="site">
                   <main id="content" className="site-content">
